46883aefc6d9006de2b3250cbf19662d0c94bc37
juseoyn <juing0105@gmail.com>
2025-04-24 15:31:14 +0900

    Add files via upload

---
+++ hw6.py
@@ -0,0 +1,10 @@
+for i in range(1, 10):
+    for j in range(2, 6):  
+        print(f"{j} x {i} = {j * i:2}", end="\t")
+    print() 
+print()  
+
+for i in range(1, 10):
+    for j in range(6, 10): 
+          print(f"{j} x {i} = {j * i:2}", end="\t")
+    print()  
